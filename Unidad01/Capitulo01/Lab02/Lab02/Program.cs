32113efa4405cc82ad2fdc383c23947126f30e57
Santiago Dedich <tanti19091@hotmail.com>
2025-03-31 14:34:12 -0300

    Clase 31-3 / Unidad 1

---
+++ Unidad01/Capitulo01/Lab02/Lab02/Program.cs
@@ -0,0 +1,9 @@
+﻿string valor1 = "Este es valor1";
+int valor2 = 5;
+
+string valor3 = valor1;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);  
+Console.WriteLine(valor3);
+Console.ReadKey();
